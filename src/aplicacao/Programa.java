c4c45b5eb3f456e2462f210d9dc0161646e4cc54
Artur Henrique Kronbauer <arturhk@gmail.com>
2023-02-09 16:44:31 -0300

    Classe Calculadora com membros de instância

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -2,28 +2,24 @@ package aplicacao;
 
 import java.util.Scanner;
 
-public class Programa {
+import util.Calculadora;
 
-	public static final double PI = 3.14159;
+public class Programa {
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Informe o raio: ");
 		double raio = sc.nextDouble();
-		double c = circumference(raio);
-		double v = volume(raio);
-		System.out.printf("Circumference: %.2f%n", c);
+		
+		Calculadora calc = new Calculadora();
+		
+		double c = calc.circunferencia(raio);
+		double v = calc.volume(raio);
+		
+		System.out.printf("Circunferência: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
-		System.out.printf("Valor do PI: %.2f%n", PI);
+		System.out.printf("Valor do PI: %.2f%n", calc.PI);
 		sc.close();
-	}
-
-	public static double circumference(double radius) {
-		return 2.0 * PI * radius;
-	}
-
-	public static double volume(double radius) {
-		return 4.0 * PI * radius * radius * radius / 3.0;
 	}
 }
